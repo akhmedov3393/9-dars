96c3979c54359ed92a9497a8828d103d779a8070
akhmedov3393 <akhmedov3393@gmail.com>
2024-01-29 10:45:09 +0500

    object

---
+++ scipts/students.js
@@ -0,0 +1,169 @@
+// const students = [
+//   {
+//     id: 1,
+//     name: "Akmal",
+//     lastName: "Ahmedov",
+//     number: "+998906283393",
+//     age: 7,
+//     sex: "male",
+//     address: "yunusobod",
+//     score: 61,
+//   },
+//   {
+//     id: 2,
+//     name: "Akbarjon",
+//     lastName: "Qahhorjonov",
+//     number: "+998903093028",
+//     sex: "male",
+//     age: 24,
+//     score: 98,
+//   },
+//   {
+//     id: 3,
+//     name: "levi",
+//     lastName: "Ahmedov",
+//     number: "+998903093028",
+//     age: 7,
+//     sex: "male",
+//     address: "yunusobod",
+//     score: 62,
+//   },
+//   {
+//     id: 4,
+//     name: "usama",
+//     lastName: "Ahmedov",
+//     number: "+998903093028",
+//     age: 8,
+//     sex: "male",
+//     address: "yunusobod",
+//     score: 69,
+//   },
+//   {
+//     id: 5,
+//     name: "grendger",
+//     lastName: "Ahmedov",
+//     number: "+998903093028",
+//     age: 7,
+//     sex: "male",
+//     address: "yunusobod",
+//     score: 60,
+//   },
+//   {
+//     id: 6,
+//     name: "susan",
+//     lastName: "Ahmedov",
+//     number: "+998903093028",
+//     age: 6,
+//     sex: "male",
+//     address: "yunusobod",
+//     score: 87,
+//   },
+//   {
+//     id: 7,
+//     name: "Sanjar",
+//     lastName: "Tursunov",
+//     number: "+998903093028",
+//     age: 9,
+//     sex: "male",
+//     address: "Yakkasaroy",
+//     score: 78,
+//   },
+//   {
+//     id: 8,
+//     name: "Mohina",
+//     lastName: "Sobirova",
+//     number: "+998903093028",
+//     age: 6,
+//     sex: "female",
+//     address: "yunusobod",
+//     score: 79,
+//   },
+//   {
+//     id: 9,
+//     name: "Umidjon",
+//     lastName: "Ergashev",
+//     number: "+998903093028",
+//     age: 7,
+//     sex: "male",
+//     address: "yunusobod",
+//     score: 42,
+//   },
+//   {
+//     id: 10,
+//     name: "Nargiza",
+//     lastName: "Abduhakimova",
+//     number: "+998903093028",
+//     age: 7,
+//     sex: "female",
+//     address: "Mirobod",
+//     score: 67,
+//   },
+//   {
+//     id: 11,
+//     name: "Sherzod",
+//     lastName: "Ikromiddinov",
+//     number: "+998903093028",
+//     age: 7,
+//     sex: "male",
+//     address: "Olmazor",
+//     score: 95,
+//   },
+//   {
+//     id: 12,
+//     name: "Asad",
+//     lastName: "Ahmedov",
+//     number: "+998903093028",
+//     age: 7,
+//     sex: "male",
+//     address: " chilonzor",
+//     score: 47,
+//   },
+//   {
+//     id: 13,
+//     name: "shahlo",
+//     lastName: "Ahmedova",
+//     number: "+998903093028",
+//     age: 6,
+//     sex: "female",
+//     address: "chilonzor",
+//     score: 59,
+//   },
+// ];
+
+// // sort by age
+
+// // let a = [];
+// // let b = [];
+
+// // for (let i = 0; i < students.length; i++) {
+// //   if (students[i].age >= 7) {
+// //     a.push(students[i].name);
+// //   } else {
+// //     b.push(students[i].name);
+// //   }
+// // }
+
+// // console.log("7 yoshdan kattalar:" + a);
+// // console.log("7 yoshdan kichkinalar:" + b);
+
+// // sort by score
+
+// let score3 = [];
+// let score4 = [];
+// let score5 = [];
+// let score2 = [];
+// for (i = 0; i < students.length; i++) {
+//   if (students[i].score < 60) {
+//     score2.push(students[i].name);
+//   } else if (students[i].score >= 60 && students[i].score < 74) {
+//     score3.push(students[i].name);
+//   } else if (students[i].score >= 74 && students[i].score < 89) {
+//     score4.push(students[i].name);
+//   } else if (students[i].score >= 90) {
+//     score5.push(students[i].name);
+//   }
+// }
+// console.log("yiqilganlar:"+score2);
+// console.log("3 ga yopganlar:"+score3);
+// console.log("4 ga yopganlar:"+score4);
+// console.log("5 ga yopganlar:"+score5);
